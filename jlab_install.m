b4bd184ce26fb9a2aa442de3b58c07f1191f0542
Jonathan M. Lilly <jonathanlilly@gmail.com>
2015-09-05 16:51:41 -0500

    Additional install instructions for GitHub download

--- jlab_install.m
+++ jlab_install.m
@@ -1,11 +1,14 @@
 %JLAB_INSTALL  Instructions for installing JLAB.
-%
+%  
 %   After unzipping, in your 'startup.m' file, you should put the lines:
 % 
 %          addpath /your/path/to/jlab
 %          jlab_addpath
 %
-%   The second line adds the various JLAB subdirectories to your search path. 
+%   The second line adds all the JLAB subdirectories to your search path. 
+%
+%   If you have downloaded from GitHub, you will need to change the top
+%   level directory to be 'jlab' instead of 'jLab-master'.  
 %  
 %   Then please type 'jlab_runtests' at the Matlab command prompt and copy
 %   and paste the output to me at eponym@jmlilly.net.  
